0d190f7265a4b1c9f16e04a0e30cbe1fdf753b84
NegativeZero06 <rameshchettiar06@gmail.com>
2025-09-17 01:27:30 +0530

    Week 4-Complete

--- Weeks/Week 2/Practise Problems/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
- public static void displayASCIITable(int start, int end) {
-        // Display ASCII codes and corresponding characters
-        for (int i = start; i <= end; i++) {
-            System.out.printf("%3d : %c   ", i, (char) i);
-            if ((i - start + 1) % 8 == 0) System.out.println();
-        }
-        System.out.println();
-    }
